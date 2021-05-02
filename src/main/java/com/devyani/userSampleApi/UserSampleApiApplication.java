449ab3e7c2c84decc34efb5ed757b01266660939
Devyani_Chauhan <34307928+Devyani37@users.noreply.github.com>
2021-05-02 13:34:54 +0200

    First Commit

---
+++ src/main/java/com/devyani/userSampleApi/UserSampleApiApplication.java
@@ -0,0 +1,13 @@
+package com.devyani.userSampleApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSampleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devyani/userSampleApi/UserSampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devyani.userSampleApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
